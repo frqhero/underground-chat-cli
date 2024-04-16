c1a822dfd3184b3c6b0d65f49aa1d64942349dde
frqhero <frqhero@gmail.com>
2024-04-16 23:42:12 +0500

    Setup logging.

--- writer.py
+++ writer.py
@@ -1,14 +1,25 @@
 import asyncio
+import logging
+
+log_format = "%(levelname)s:%(name)s:%(message)s"
+formatter = logging.Formatter(log_format)
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+logger = logging.getLogger('writer')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 
 async def tcp_echo_client(host, port, token):
     reader, writer = await asyncio.open_connection(host, port)
     reply = await reader.read(200)
+    logger.debug(reply.decode())
 
     writer.write((token + '\n').encode())
     await writer.drain()
 
     reply = await reader.read(200)
+    logger.debug(reply.decode())
 
     message = 'Hello, minechat!'
     writer.write((message + '\n\n').encode())
