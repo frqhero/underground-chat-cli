f0795b6f950a79d012ff3811b539476c7bf6af9e
frqhero <frqhero@gmail.com>
2024-04-18 00:57:07 +0500

    Add CLI options to sender script

--- sender.py
+++ sender.py
@@ -1,3 +1,4 @@
+import argparse
 import asyncio
 import json
 import logging
@@ -63,24 +64,66 @@ async def submit_message(writer, message):
 
     writer.write((message + '\n\n').encode())
     await writer.drain()
+    logger.debug(f'Message sent: {message}')
+
+
+def create_parser():
+    parser = argparse.ArgumentParser(
+        description='This script is used for serving a microservice'
+    )
+    parser.add_argument(
+        '--token',
+        default='',
+        type=str,
+        help='Set token for authorization',
+    )
+    parser.add_argument(
+        '--host',
+        default='minechat.dvmn.org',
+        type=str,
+        help='Set host for connection',
+    )
+    parser.add_argument(
+        '--port',
+        default='5050',
+        type=str,
+        help='Set port for connection',
+    )
+    parser.add_argument(
+        '--name',
+        default='user',
+        type=str,
+        help='Set nickname for registration',
+    )
+    parser.add_argument(
+        '--message',
+        type=str,
+        help='Set message for sending',
+        required=True,
+    )
+    return parser
 
 
 def main():
-    host = 'minechat.dvmn.org'
-    port = 5050
-    nickname = 'idk'
-    message = 'Hello, world!'
+    parser = create_parser()
+    parser_args = parser.parse_args()
 
-    with open('token.txt', 'r') as file:
-        token = file.readline().strip()
+    host = parser_args.host
+    port = parser_args.port
+    nickname = parser_args.name
+    message = parser_args.message
+    token = parser_args.token
+
+    if not token:
+        with open('token.txt', 'r') as file:
+            token = file.readline().strip()
     if not token:
         token = asyncio.run(register(host, port, nickname))
         with open('token.txt', 'w') as file:
             file.write(token)
-    else:
-        writer = asyncio.run(authorize(host, port, token))
-        if not writer:
-            raise ConnectionError('Authorization failed')
+    writer = asyncio.run(authorize(host, port, token))
+    if not writer:
+        raise ConnectionError('Authorization failed')
     asyncio.run(submit_message(writer, message))
 
 
